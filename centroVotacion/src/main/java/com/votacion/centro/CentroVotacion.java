2e5e1abf0e03381437586271291f8bc33c820635
Tomás <tomasquin2003@gmail.com>
2025-06-01 22:50:10 -0500

    Initial Commit Base Project Structure - Gradle - Java - Success Running

---
+++ centroVotacion/src/main/java/com/votacion/centro/CentroVotacion.java
@@ -0,0 +1,7 @@
+package com.votacion.centro;
+
+public class CentroVotacion {
+    public static void main(String[] args) {
+        System.out.println("Centro de Votación - Inicializando...");
+    }
+}
